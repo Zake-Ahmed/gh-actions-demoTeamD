99d80a65a5a8e11df4b3e1a41db6227dd840f581
Zake-Ahmed <zake3012@gmail.com>
2022-04-28 18:18:42 +0100

    Cal test added

--- test-cal.sh
+++ test-cal.sh
@@ -1,23 +1,32 @@
+#!/bin/bash
 declare -a num1s=(1 23 5 7 42 18)
 declare -a num2s=(3 44 15 26 30 1)
-
+declare -a c=(+ - /)
 declare -A results=()
-for i in {0..5}; do
-  a=${num1s[$i]}
-  b=${num2s[$i]}
-  results[$a,$b]=$(($a + $b))
-  results2[$a,$b]=$(($a - $b))
-  results3[$a,$b]=$(($a / $b))
-  results4[$a,$b]=$(($a * $b))
-done
+for j in {0..2};do
+  c=${c[$j]}
+  for i in {0..5}; do
+    a=${num1s[$i]}
+    b=${num2s[$i]}
+    results[$a,$b]=$(($a $c $b))
 
+    if [  $(./cal.sh $a $b $c) -eq ${results[$a,$b]}  ]; then
+         echo "Test passed for $c"
+    else
+      echo "Test failed"
+      exit 1
+    fi
+  done
+done
 for i in {0..5}; do
-  a=${num1s[$i]}
-  b=${num2s[$i]}
-  if [ [ $(./add.sh $a $b ) -eq ${results[$a,$b]}  &&  $(./sub.sh $a $b) -eq ${results2[$a,$b]}  &&  $(./div.sh $a $b) -eq ${results3[$a,$b]}  &&  $(./multi.sh $a $b) -eq ${results4[$a,$b]} ] ]; then
-       echo "Test passed"
+ a=${num1s[$i]}
+ b=${num2s[$i]}
+ results[$a,$b]=$(($a * $b))
+
+  if [  $(./multi.sh $a $b) -eq ${results[$a,$b]}  ]; then
+      echo "Test passed for *"
   else
-    echo "Test failed"
-    exit 1
+      echo "Test failed"
+      exit 1
   fi
 done
