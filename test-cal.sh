e64b2050c477b8551170834577de326fbaa1a569
Zake-Ahmed <zake3012@gmail.com>
2022-04-28 16:11:33 +0100

    added cal

---
+++ test-cal.sh
@@ -0,0 +1,26 @@
+declare -a num1s=(1 23 5 7 42 18)
+declare -a num2s=(3 44 15 26 30 1)
+
+declare -A results=()
+for i in {0..5}; do
+  a=${num1s[$i]}
+  b=${num2s[$i]}
+  results[$a,$b]=$(($a + $b))
+  results2[$a,$b]=$(($a - $b))
+  results3[$a,$b]=$(($a / $b))
+  results4[$a,$b]=$(($a * $b))
+done
+
+for i in {0..5}; do
+  a=${num1s[$i]}
+  b=${num2s[$i]}
+  if [ $(./add.sh $a $b ) -eq ${results[$a,$b]} ]; then
+  if [ $(./sub.sh $a $b) -eq ${results2[$a,$b]} ]; then
+  if [ $(./div.sh $a $b) -eq ${results3[$a,$b]} ]; then
+  if [ $(./multi.sh $a $b) -eq ${results4[$a,$b]} ]; then
+  echo "Test passed"
+  else
+    echo "Test failed"
+    exit 1
+  fi
+done
