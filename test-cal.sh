620e46973c78448a6e9b60cb693aaf76a0eba376
Zake-Ahmed <zake3012@gmail.com>
2022-04-29 09:24:19 +0100

    last commit

--- test-cal.sh
+++ test-cal.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 declare -a num1s=(1 23 5 7 42 18)
 declare -a num2s=(3 44 15 26 30 1)
-declare -a c=(+ - /)
+declare -a c=(+ - / \*)
 declare -A results=()
-for j in {0..2};do
+for j in {0..3};do
   c=${c[$j]}
   for i in {0..5}; do
     a=${num1s[$i]}
@@ -18,15 +18,4 @@ for j in {0..2};do
     fi
   done
 done
-for i in {0..5}; do
- a=${num1s[$i]}
- b=${num2s[$i]}
- results[$a,$b]=$(($a * $b))
 
-  if [  $(./multi.sh $a $b) -eq ${results[$a,$b]}  ]; then
-      echo "Test passed for *"
-  else
-      echo "Test failed"
-      exit 1
-  fi
-done
